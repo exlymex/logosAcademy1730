6d66f7dd764801483d41a8d7e1bf5496404073d7
email <pavloborisov44@gmail.com>
2022-06-05 11:59:29 +0300

    filter created

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -10,7 +10,9 @@ const storage = multer.diskStorage({
 
     },
     filename:function (req,file,cb){
-        cb(null,new Date() + file.originalname)
+        const parts = file.mimetype.split('/')
+        const ext = `.` + parts[parts.length - 1]
+        cb(null,req.user.userId + ext)
     }
 })
 const fileFilter = (req,file,cb) => {
@@ -32,7 +34,7 @@ const upload = multer({
 
 router.get('/users', authMiddleware,async (req, res) => {
     try {
-        const users = await User.find({},{username:1,email:1})
+        const users = await User.find({},{username:1,email:1,userImage:1})
         res.status(201).json({users})
     } catch (e) {
         res.status(500).json({
@@ -44,10 +46,12 @@ router.get('/details', authMiddleware,async (req, res) => {
     try {
         const {userId} = req.query
         const user = await User.findById(userId,{ username:0,email:0,password:0})
+
+        user.userImage = `http://localhost:5000/uploads/${user.userImage}`
         if(user){
             return res.status(201).json({user})
         }
-        res.status(404).send()
+        res.status(404).send('Not found')
     } catch (e) {
         res.status(500).json({
             message: 'Something went wrong, try again later'
@@ -56,51 +60,16 @@ router.get('/details', authMiddleware,async (req, res) => {
 })
 router.post('/details/post',authMiddleware,async (req,res) => {
     try{
-        const {userId} = req.body
         if(!req.body.detail){
             return res.status(400).json( {
                 message:'Bad details'
             })
         }
-        if (userId !== req.user.userId) {
-            return res.status(400).json({
-                message: 'Ви не маєте право міняти чужі поля'
-            })
-        }
-        await User.findOneAndUpdate({_id:userId},{...req.body.detail})
+        await User.findOneAndUpdate({_id:req.user.userId},{...req.body.detail})
         res.status(201).json({
             message:'Change is completed'
         })
     }
-    // ,{userImage:req.file.path} ,upload.single('userImage')
-    catch(e){
-        res.status(500).json({
-            message: 'Something went wrong, try again later'
-        })
-    }
-})
-router.post('/follow',authMiddleware,async (req,res) => {
-    try{
-        const {id} = req.body
-        if(!id || id === req.user.userId) {
-            return res.status(400).json({
-                message: 'Помилка'
-            })
-        }
-        const user = await User.findById(req.user.userId)
-        console.log(user.following.includes(id))
-        if(!user.following.includes(id)){
-            user.following = [...user.following,id]
-            await user.save()
-            return res.status(201).json([...user.following,id])
-        }
-        return res.status(400).json({
-            message: 'Помилка'
-        })
-        // const user = await User.findOneAndUpdate({_id:req.user.userId},{$push:{following : id }})
-
-    }
-        // ,{userImage:req.file.path} ,upload.single('userImage')
     catch(e){
         res.status(500).json({
             message: 'Something went wrong, try again later'
@@ -121,4 +90,75 @@ router.get('/follow/get',authMiddleware,async (req,res) => {
         })
     }
 })
+router.post('/follow',authMiddleware,async (req,res) => {
+    try{
+        const {id} = req.body
+        if(!id || id === req.user.userId) {
+            return res.status(400).json({
+                message: 'Помилка'
+            })
+        }
+        const user = await User.findById(req.user.userId)
+        if(!user.following.includes(id)){
+            user.following = [...user.following,id]
+            await user.save()
+            return res.status(201).json([...user.following,id])
+        }
+        return res.status(400).json({
+            message: 'Помилка'
+        })
+        // const user = await User.findOneAndUpdate({_id:req.user.userId},{$push:{following : id }})
+
+    }
+        // ,{userImage:req.file.path} ,upload.single('userImage')
+    catch(e){
+        res.status(500).json({
+            message: 'Something went wrong, try again later'
+        })
+    }
+})
+router.post('/unfollow',authMiddleware,async (req,res) => {
+    try{
+        const {id} = req.body
+        if(!id || id === req.user.userId) {
+            return res.status(400).json({
+                message: 'Помилка'
+            })
+        }
+        const user = await User.findById(req.user.userId)
+        if(user.following.includes(id)){
+            user.following = user.following.splice(id,1)
+            await user.save()
+            return res.status(201).json([...user.following,id])
+        }
+        return res.status(400).json({
+            message: 'Помилка'
+        })
+
+    }
+    catch(e){
+        res.status(500).json({
+            message: 'Something went wrong, try again later'
+        })
+    }
+})
+
+router.post('/image/post',[authMiddleware,upload.single('file')],async (req,res) => {
+    try{
+        const user = await User.findById(req.user.userId)
+        user.userImage = req.file.filename
+        await user.save()
+        const obj = {
+            avatar:`http://localhost:5000/uploads/${user.userImage}`
+        }
+        return res.status(201).json(obj)
+    }
+    catch(e){
+        res.status(500).json({
+            message: 'Something went wrong, try again later'
+        })
+    }
+})
+
+
 module.exports = router
